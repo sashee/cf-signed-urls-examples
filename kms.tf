1ceeb5f81e184f5ae3065218256e8abacaf723eb
sashee <tamas.sallai@advancedweb.hu>
2023-07-12 16:20:17 +0200

    simplify

--- kms.tf
+++ kms.tf
@@ -4,10 +4,8 @@ resource "aws_kms_key" "key" {
 resource "terraform_data" "gen_keys" {
   provisioner "local-exec" {
     command = <<EOT
-RES=$(aws kms generate-data-key-pair-without-plaintext --key-id $KEY_ARN --key-pair-spec RSA_2048 --encryption-context "USE=CF")
-echo "{\"PrivateKeyCiphertextBlob\": \"$(echo $RES | jq -r '.PrivateKeyCiphertextBlob')\", \"PublicKey\": \"$(echo $RES | jq -r '.PublicKey')\"}" > $TARGET_FILE
+aws kms generate-data-key-pair-without-plaintext --key-id $KEY_ARN --key-pair-spec RSA_2048 --encryption-context "USE=CF" > $TARGET_FILE
 EOT
-		interpreter = ["bash", "-c"]
 		environment = {
 			KEY_ARN = aws_kms_key.key.arn
 			TARGET_FILE = "/tmp/keys-${random_id.id.hex}.json"
